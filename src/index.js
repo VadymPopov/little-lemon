b45f8cf53aa9089857e8ac016d3ef5adf0d1051e
VadymPopov <boycristal10@gmail.com>
2024-06-29 19:06:57 -0400

    normalize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "modern-normalize/modern-normalize.css";
 import { BrowserRouter } from "react-router-dom";
+import "modern-normalize/modern-normalize.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
